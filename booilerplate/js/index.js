21fd1777dff43a67b3ae7cc197eb6b29e3b26b10
ArielTheFirstGen-Economist <165543901+ArielTheFirstGen-Economist@users.noreply.github.com>
2026-02-22 19:37:41 -0800

    form and functionallity added

--- booilerplate/js/index.js
+++ booilerplate/js/index.js
@@ -8,7 +8,7 @@ banner.className = 'addBanner';
 banner.textContent = 'This is a test';
 banner.setAttribute(
     'style', 
-    'color: black; background: #87CEFA; margin: 2rem 0');
+    'color: black; background: green; margin: 2rem 0');
 
 newBanner.prepend(banner);
 
@@ -58,3 +58,51 @@ for (let i = 0;
         skill.textContent = `${skills[i]}`;
         skillsList.append(skill)
     } 
+
+/* Handling Message Form */
+
+
+// We must select the DOM element 
+// Since it is a form submit will automciallty be detected
+const messageForm = document.querySelector('form[name="leave_message"]');
+
+
+messageForm.addEventListener('submit', function (e) {
+    event.preventDefault();
+    
+    console.log(e);  
+    
+        const nameInput = event.target.usersNames;
+        const emailInput = event.target.usersEmail;
+        const messageInput = event.target.usersMessage;
+        
+        console.log(`Hello ${nameInput.value}`);
+        console.log(`Email is ${emailInput.value}`);
+        console.log(`Message is ${messageInput.value}`);
+    
+    const messageSelection = document.querySelector('#messages');
+    const messageList = messageSelection.querySelector('ul');
+    const newMessage = document.createElement('li');
+        newMessage.innerHTML = `
+        <a href="mailto:${emailInput.value}">${nameInput.value}</a>
+        <span>${messageInput.value}</span>
+        `; 
+    /* alert('Your form was submitted!') */
+    messageList.appendChild(newMessage);
+    /* messageForm.reset(); */
+});
+
+/* Remove button */
+
+const addBtn = document.querySelector('#messages')
+const removeButton = document.createElement('button');
+        removeButton.innerHTML = "<a>Remove</a>"
+            addBtn.appendChild(removeButton);
+
+removeButton.addEventListener('click', function (e) {
+        console.log(e)
+        const entry = removeButton.parentNode;
+
+        console.log(entry);
+        entry.remove();
+});
